20989bbb446e7f67cbf0f2a214348bc264847ad5
cjbrunnen <19531715+cjbrunnen@users.noreply.github.com>
2020-11-29 09:02:03 +0000

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './styles.scss'
+
+export default function App() {
+  return (
+    <div className="App">
+      <div class="spinner">
+        <span class="shape shape-1" />
+        <span class="shape shape-2" />
+      </div>
+    </div>
+  )
+}
